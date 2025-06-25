a6dd1c9b07c67855a699e45e8ef1f92f5fb31405
Pushkar Jaiswal <pushkarbst90@gmail.com>
2025-06-25 16:10:09 +0530

    Added base url for new  backend api

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,12 +1,13 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({});
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
     console.log("=== API CONNECTOR DEBUG ===");
     console.log("Method:", method);
     console.log("URL:", url);
-    console.log("Full URL:", url.startsWith('http') ? url : `http://localhost:4000${url}`);
+    console.log("Full URL:", url.startsWith('http') ? url : `${BASE_URL}${url}`);
     console.log("Body Data:", bodyData);
     console.log("Headers:", headers);
     
