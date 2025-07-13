a0c8aebec753e382f89b2d15114bc3159764235c
Pushkar Jaiswal <pushkarbst90@gmail.com>
2025-07-13 22:39:08 +0530

    add debug log for REACT_APP_BASE_URL in apiconnector.js

--- src/services/apiconnector.js
+++ src/services/apiconnector.js
@@ -1,5 +1,7 @@
 import axios from "axios"
 
+console.log('REACT_APP_BASE_URL:', process.env.REACT_APP_BASE_URL);
+
 export const axiosInstance = axios.create({});
 const BASE_URL = process.env.REACT_APP_BASE_URL
   ? process.env.REACT_APP_BASE_URL
